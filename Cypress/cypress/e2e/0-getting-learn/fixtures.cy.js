237869f9b5f8cbbcfbddd4de0eb29965fb88c6ff
Dewi Ayu <deasa092@gmail.com>
2023-10-03 21:10:12 +0700

    Upload program automation menggunakan fixtures pada website zero

---
+++ Cypress/cypress/e2e/0-getting-learn/fixtures.cy.js
@@ -0,0 +1,27 @@
+describe('S Browser Checking', () => {
+    it('Open the website', () => {
+       cy.visit('http://zero.webappsecurity.com/login.html')
+       cy.fixture("user").then(user =>{
+            const username = user.username1
+            const password = user.password
+            const cari = user.cari
+            const name = user.name
+            const email = user.email
+            const subject = user.subject
+            const pesan = user.pesan
+            cy.get('#user_login').type(username)
+            cy.get('#user_password').type(password)
+            cy.get("input[value='Sign in']").click()
+            cy.get('#searchTerm').type(cari)
+            cy.get('#searchTerm').type('{enter}');
+            cy.get("li[id='feedback'] div strong").click()
+            cy.get('#name').type(name)
+            cy.get('#email').type(email)
+            cy.get('#subject').type(subject)
+            cy.get('#comment').type(pesan)
+            cy.get("input[value='Send Message']").click()
+
+       })
+       
+       });
+  })
